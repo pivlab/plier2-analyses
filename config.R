b885065e473820e24cc64915547c99c1e2341a67
Milton Pividori <miltondp@gmail.com>
2025-05-04 01:14:05 -0600

    archs4: split rmd into data download and data preprocess

---
+++ config.R
@@ -0,0 +1,30 @@
+library(bigstatsr)
+library(here)
+
+config <- list()
+
+config$GENERAL <- list(
+  N_CORES=nb_cores() %/% 4,
+  CHUNK_SIZE=100,
+  DATA_DIR=here(file.path("data")),
+  OUTPUT_DIR=here(file.path("output"))
+)
+  
+config$ARCHS4=list(
+  DATASET_NAME="archs4",
+  DATASET_FOLDER=here(
+    file.path(config$GENERAL$OUTPUT_DIR, "archs4")
+  ),
+  DATASET_ENSEMBL_VERSION=107,
+  URL="https://s3.dev.maayanlab.cloud/archs4/files/human_gene_v2.5.h5",
+  DATASET_FILE=here(file.path("data", "archs4", "human_gene_v2.5.h5")),
+  GENES_MEAN_CUTOFF=0.5,
+  GENES_VAR_CUTOFF=0.1,
+  PLIER_PARAMS=list(
+    K=942,
+    MULTIPLIER=3,
+    MAX_U_UPDATES=3,
+    MAX_ITER=350,
+    N_CORES=1
+  )
+)
