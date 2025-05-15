d671596d2b86daeded6631667157a89f5dd95cb4
msubirana <mb2subi@gmail.com>
2025-05-15 10:13:15 -0600

    new folder strucutre and upload gtex rmd

--- config.R
+++ config.R
@@ -29,3 +29,23 @@ config$ARCHS4=list(
     N_CORES=1
   )
 )
+
+config$GTEx=list(
+  DATASET_NAME="gtex",
+  DATASET_FOLDER=here(
+    file.path(config$GENERAL$OUTPUT_DIR, "gtex")
+  ),
+  DATASET_ENSEMBL_VERSION=107,
+  URL="https://storage.googleapis.com/adult-gtex/bulk-gex/v8/rna-seq/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.gct.gz",
+  DATASET_FILE=here(file.path("data", "gtex", "bulk-gex_v8_rna-seq_GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_tpm.rds")),
+  GENES_MEAN_CUTOFF=1,
+  GENES_VAR_CUTOFF=0.1,
+  PLIER_PARAMS=list(
+    RANDOM_SVD_N_CORES=30,
+    RANDOM_SVD_SEED=123,
+    MULTIPLIER=3,
+    MAX_U_UPDATES=3,
+    MAX_ITER=350,
+    N_CORES=1
+  )
+)
